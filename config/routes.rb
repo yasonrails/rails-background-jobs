cefc993e3806bf22281e6331bc36932a6ee1118d
yas <yasbernays@gmail.com>
2022-10-18 20:16:37 +0200

    sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  require "sidekiq/web"
+  authenticate :user, ->(user) { user.admin? } do
+    mount Sidekiq::Web => '/sidekiq'
   # Defines the root path route ("/")
   # root "articles#index"
 end
